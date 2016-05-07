4070bd25dcfdb13c61cdcd5f3b890be8618c5172
Richard Guest <r.guest@gns.cri.nz>
2016-05-08 09:38:31 +1200

    Update installation to v2.

--- createnode.sh
+++ createnode.sh
@@ -2,9 +2,9 @@
 
 if [ ! -e /etc/sd-agent/config.cfg ]; then
   cd /tmp
-  ./agent-install.sh -a https://$ACCOUNT_NAME.serverdensity.io -t $API_KEY -g $GROUPNAME
+  ./agent-install.sh -a $ACCOUNT_NAME -t $API_KEY -g $GROUPNAME || true
 fi
 
-if [ -e /var/log/sd-agent/sd-agent.pid ]; then
-  rm /var/log/sd-agent/sd-agent.pid
+if [ -e /var/run/sd-agent/sd-agent.pid ]; then
+  rm /var/run/sd-agent/sd-agent.pid
 fi 
