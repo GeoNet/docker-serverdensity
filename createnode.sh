172cdf64fd33a2b013daeda889643468b5f1b4c1
Richard Guest <r.guest@gns.cri.nz>
2016-05-10 13:46:59 +1200

    Add docker monitoring.

--- createnode.sh
+++ createnode.sh
@@ -5,6 +5,16 @@ if [ ! -e /etc/sd-agent/config.cfg ]; then
   ./agent-install.sh -a $ACCOUNT_NAME -t $API_KEY -g $GROUPNAME || true
 fi
 
+if [ ! -e /etc/sd-agent/conf.d/docker.yml ]; then
+  yum install -y sd-agent-docker
+  cat > /etc/sd-agent/conf.d/docker.yaml << EOF
+init_config:
+  docker_root: /
+instances:
+  - url: "unix://var/run/docker.sock"
+EOF
+fi
+
 if [ -e /var/run/sd-agent/sd-agent.pid ]; then
   rm /var/run/sd-agent/sd-agent.pid
 fi 
